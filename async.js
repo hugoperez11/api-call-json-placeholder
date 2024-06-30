7f03ba52b5b4f61852d95ded0f17dabe63eec99f
Hugo Perez Santos <hugopsantos@hotmail.com>
2024-06-30 19:09:16 +0200

    first commit

--- async.js
+++ async.js
@@ -1,28 +1,19 @@
-//constante path que contiene la ruta al archivo JSON (data.json)
-const path = 'data.json';
 
-async function app() {
-    // Llama a la función retrieveData y espera a que complete su ejecución para asignar el resultado a la constante data.    await se usa porque retrieveData devuelve una promesa.
-    const data = await retrieveData();
-    renderDataInconsole(data);
+const uri= 'https://jsonplaceholder.typicode.com/users';
+
+// Función asíncrona para obtener y mostrar los datos de los usuarios
+async function getData() {
+  try {
+    // Realiza la solicitud a la API
+    const response = await fetch(uri);  
+    
+    // Convierte la respuesta a JSON
+    const users = await response.json();   
+    console.log(users);
+  
+
+  } catch (error) {    
+    console.error('Error in your fetch operation:', error);
+  }
 }
-
-async function retrieveData() {
-    //Usa fetch para hacer una solicitud HTTP GET a la ruta definida en path.    await se usa porque fetch devuelve una promesa.
-    const response = await fetch(path);
-    //Convierte la respuesta de la solicitud en un objeto JSON.    await se usa porque .json() devuelve una promesa.
-    const data = await response.json();
-    return data;
-}
-
-function renderDataInconsole(data) {
-    for (let index = 0; index < data.results.length; index++) {
-        console.log(data.results[index].title);
-    }
-}
-
-
-app();
-
-
-
+getData();
--- asynctable.js
+++ asynctable.js
@@ -1,30 +1,52 @@
-const path = 'data.json';
+const uri = 'https://jsonplaceholder.typicode.com/users';
 
+// Función principal que se llama al cargar la página
 async function app() {
     const data = await retrieveData();
     renderDataInTable(data);
 }
-
+ 
+// Función para recuperar los datos de todos los usuarios
 async function retrieveData() {
-    const response = await fetch(path);
+    const response = await fetch(uri);
     const data = await response.json();
     return data;
 }
 
 function renderDataInTable(data) {
     const tableBody = document.getElementById('todosTable').getElementsByTagName('tbody')[0];
-    data.results.forEach(todo => {
+    data.forEach(todo => {
         //inserta una nueva fila al final del <tbody> y devuelve una referencia a la nueva fila (elemento <tr>).
         const row = tableBody.insertRow();
         // inserta una nueva celda en la primera posición de la fila y devuelve una referencia a esta celda (elemento <td>).
-        const cellTitle = row.insertCell(0);
-        const cellPriority = row.insertCell(1);
-        const cellIsDone = row.insertCell(2);
+        const cellId = row.insertCell(0);
+        const cellName = row.insertCell(1);
+        const cellCity = row.insertCell(2);
         //textContent es una propiedad de los elementos del DOM (Document Object Model) en JavaScript que representa el contenido textual de un nodo y de todos sus descendientes.
-        cellTitle.textContent = todo.title;
-        cellPriority.textContent = todo.priority;
-        cellIsDone.textContent = todo.isDone ? 'Yes' : 'No';
+        cellId.textContent = todo.id;
+        cellName.textContent = todo.username;
+        cellCity.textContent = todo.address.city;
     });
+};
+
+// Función para recuperar los datos de un usuario por su ID
+async function UserById(id) {
+    const response = await fetch(`${uri}/${id}`);   
+    const user = await response.json();
+        render(user);   
 }
 
+// Función para mostrar la información del usuario en el DOM
+function render(user) {
+    const userInfoDiv = document.getElementById('userInfo');      
+    userInfoDiv.innerHTML = `<p>Name: ${user.name}</p><p>Phone: ${user.phone}</p>`;   
+}
+
+// Event listener para el botón de búsqueda de usuario
+document.getElementById('UserButton').addEventListener('click', () => {
+    const userId = document.getElementById('userId').value;    
+        UserById(userId);    
+});
+
+// Llama a la función principal para iniciar el proceso
 app();
